5062d4175af570c348b00eb5861d8d00a867103a
Lucas Branco <lucasbranco544@gmail.com>
2025-06-07 18:33:12 -0300

    Escondendo senhas e .env

--- models.py
+++ models.py
@@ -1,8 +1,17 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import declarative_base
 
+load_dotenv()
+
+db_username = os.getenv("DB_USERNAME")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
 #cria a conexão com o banco de dados
-db = create_engine("mysql+pymysql://root:123test@localhost:3306/projeto")
+db = create_engine(f"mysql+pymysql://{db_username}:{db_password}@"f"{db_host}:{db_port}/{db_name}")
 
 #cria a base do banco de dados
 Base = declarative_base()
